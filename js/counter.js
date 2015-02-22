87cf951051d87ec4a1098c4ab878bc6f1c6f62f1
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-22 11:59:22 +0100

    create a model

--- js/counter.js
+++ js/counter.js
@@ -1,3 +1,8 @@
 ;(function($){
     /* create your demo here! */
+    var demoApp = angular.module('demoApp', []);
+
+    demoApp.controller("demoController", function($scope){
+        $scope.count = 0;
+    });
 })(window.demo = window.demo || {});
