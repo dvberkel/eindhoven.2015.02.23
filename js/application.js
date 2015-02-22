d296e91ea7061ac7c7758cef34ff7c4b956f4131
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-22 20:00:56 +0100

    introduce partials
    For partials to work the application should be served by a server
    
    Run one with the command `python -mSimpleHTTPServer 8000`

--- js/application.js
+++ js/application.js
@@ -20,13 +20,7 @@
 
     kwetterApp.directive('tweet', function(){
         return {
-            'template': '<div class="tweet col-md-8 col-md-offset-2">' +
-          '<div class="content">' +
-            '<img class="avatar" src="{{tweet.avatar}}" alt="">' +
-            '<p class="tweet-text" >{{tweet.content}}</p>' +
-          '</div>' +
-          '<div class="info"><span class="favourite">{{tweet.favourite}}</span></div>' +
-        '</div>'
+            'templateUrl': 'partials/tweet.html'
         }
     });
 })();
