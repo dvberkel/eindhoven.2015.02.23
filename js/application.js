b790e4c21d2f7e001588e1b93136a7e8f9f9a261
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-22 19:20:23 +0100

    create a model

--- js/application.js
+++ js/application.js
@@ -1,3 +1,12 @@
 ;(function(){
     console.log('Write your application here!');
+    var kwetterApp = angular.module('kwetterApp', []);
+
+    kwetterApp.controller('timeline', function($scope){
+        $scope.tweet = {
+            'avatar': 'image/placeholder2.gif',
+            'content': 'Kittens are awesome!',
+            'favourite': 51
+        };
+    });
 })();
