442f1f5c10ddd2d521eab94ab3e3b6999c58c462
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-22 19:46:59 +0100

    introduce a directive

--- js/application.js
+++ js/application.js
@@ -17,4 +17,16 @@
             aTweet('Kittens are stupid', 0, 'image/placeholder3.gif')
         ];
     });
+
+    kwetterApp.directive('tweet', function(){
+        return {
+            'template': '<div class="tweet col-md-8 col-md-offset-2">' +
+          '<div class="content">' +
+            '<img class="avatar" src="{{tweet.avatar}}" alt="">' +
+            '<p class="tweet-text" >{{tweet.content}}</p>' +
+          '</div>' +
+          '<div class="info"><span class="favourite">{{tweet.favourite}}</span></div>' +
+        '</div>'
+        }
+    });
 })();
